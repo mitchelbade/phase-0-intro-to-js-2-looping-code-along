27af0a64a6c0095aa97fb3b325d3128f3f569022
mitchelbade <mitchelm.bade@gmail.com>
2021-11-08 21:37:50 -0800

    commit tree ea6f4f149e725d771f0492ed8bab7596d60228e5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+    }
+
+    return newArr;
+}
+
+function countDown(int) {
+    let i = 11;
+    while (i > 0) {
+        i--
+        console.log(i);
+    debugger;
+    }
+
+    return int;
+}
